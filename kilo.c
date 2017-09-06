3f0bf4b88f6ec71798300d243420efb98bcd9ca9
algebroid <algebroid0@gmail.com>
2017-09-06 22:18:44 +0900

    turn off echoing.

--- kilo.c
+++ kilo.c
@@ -1,6 +1,17 @@
+#include <termios.h>
 #include <unistd.h>
+
+void enableRawModee() {
+  struct termios raw;
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+  enableRawModee();
+
   char c;
-  while(read(STDIN_FILENO, &c, 1) == 1);
+  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
 }
