5d028140809c9b8c02ede644abb569ac361256fa
algebroid <algebroid0@gmail.com>
2017-09-07 20:32:01 +0900

    disable some flags.

--- kilo.c
+++ kilo.c
@@ -15,7 +15,10 @@ void enableRawMode() {
   atexit(disableRawMode);
 
   tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -25,9 +28,9 @@ int main() {
   char c;
   while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
     if (iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
   }
   return 0;
