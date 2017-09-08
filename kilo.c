32d16d090e8055ded2e37dd0d75753aad1f771f3
algebroid <algebroid0@gmail.com>
2017-09-08 23:19:30 +0900

    chapter 3.1: Press `Ctrl-Q` to quit

--- kilo.c
+++ kilo.c
@@ -5,6 +5,8 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 struct termios orig_termios;
 
 void die(const char *s) {
@@ -42,7 +44,7 @@ int main() {
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
-    if (c == 'q') break;
+    if (c == CTRL_KEY('q')) break;
   }
   return 0;
 }
