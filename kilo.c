cf1af4e7492cf84b50581cc024dfd5db03f590a2
algebroid <algebroid0@gmail.com>
2017-09-11 00:13:09 +0900

    Get Windows size by easy way.

--- kilo.c
+++ kilo.c
@@ -2,12 +2,15 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 struct editorConfig {
+  int screenrows;
+  int screencols;
   struct termios orig_termios;
 };
 
@@ -42,7 +45,7 @@ void enableRawMode() {
 
 void editorDrawRows() {
   int y;
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
@@ -64,6 +67,16 @@ char editorReadKey() {
   return c;
 }
 
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return 0;
+  }
+  *cols = ws.ws_col;
+  *rows = ws.ws_row;
+  return 0;
+}
+
 void editorProcessKeyPress() {
   char c = editorReadKey();
 
@@ -76,8 +89,13 @@ void editorProcessKeyPress() {
   }
 }
 
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
   enableRawMode();
+  initEditor();
 
   while(1) {
     editorRefreshScreen();
