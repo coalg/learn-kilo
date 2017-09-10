d07441e235feeb17c749e95f33d38ca3bdbe8d1c
algebroid <algebroid0@gmail.com>
2017-09-10 11:51:15 +0900

    cleanup keyboard input.

--- kilo.c
+++ kilo.c
@@ -33,18 +33,30 @@ void enableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+  while((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+void editorProcessKeyPress() {
+  char c = editorReadKey();
+
+  switch(c) {
+    case CTRL_KEY('q'):
+      exit(0);
+      break;
+  }
+}
+
 int main() {
   enableRawMode();
 
   while(1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == CTRL_KEY('q')) break;
+    editorProcessKeyPress();
   }
   return 0;
 }
