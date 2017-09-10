6ec58899c5e0654198b214ad51f057e3ca9a0a34
algebroid <algebroid0@gmail.com>
2017-09-10 14:04:04 +0900

    clear cursor on exit.

--- kilo.c
+++ kilo.c
@@ -10,6 +10,9 @@
 struct termios orig_termios;
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
@@ -52,6 +55,8 @@ void editorProcessKeyPress() {
 
   switch(c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
