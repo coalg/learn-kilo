ccadbbb0fc9171eb2f1862cb0db62fae75737fa8
algebroid <algebroid0@gmail.com>
2017-09-11 00:02:22 +0900

    add vim-like column of tildes.

--- kilo.c
+++ kilo.c
@@ -36,8 +36,18 @@ void enableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
 void editorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
