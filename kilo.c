3a2ce40dc54aea486aea65344dd88be9985d257f
algebroid <algebroid0@gmail.com>
2017-09-12 07:23:06 +0900

    reading characters into a buffer.

--- kilo.c
+++ kilo.c
@@ -68,17 +68,20 @@ char editorReadKey() {
 }
 
 int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
   if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
 
   printf("\r\n");
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1) {
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 4) != 4) break;
+    if (buf[i] == 'R') break;
+    i++;
   }
+  buf[i] = '\0';
 
   editorReadKey();
   return -1;
