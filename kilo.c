a463e17e9efb1ce25099fcdb6f0d5f12d806e6bc
algebroid <algebroid0@gmail.com>
2017-09-17 11:01:58 +0900

    Welcome message

--- kilo.c
+++ kilo.c
@@ -7,6 +7,8 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define KILO_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 struct editorConfig {
@@ -105,12 +107,18 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "~", 3);
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+          "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
 
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
-      write(STDOUT_FILENO, "\r\n", 2);
       abAppend(ab, "\r\n", 2);
     }
   }
