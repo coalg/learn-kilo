83544d9d44620fd3b24a0625ea6e621c89e69c13
algebroid <algebroid0@gmail.com>
2017-09-18 00:12:35 +0900

    display first line of file

--- kilo.c
+++ kilo.c
@@ -147,15 +147,28 @@ int getWindowSize(int *rows, int *cols) {
   return 0;
 }
 
-void editorOpen() {
-  char *line = "Hello, World!";
-  ssize_t linelen = 13;
+void editorOpen(char* filename) {
+  FILE *fp = fopen(filename, "r");
+  if(!fp) die("fopen");
 
-  E.row.size = linelen;
-  E.row.chars = malloc(linelen + 1);
-  memcpy(E.row.chars, line, linelen);
-  E.row.chars[linelen] = '\0';
-  E.numrows = 1;
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  if (linelen != -1) {
+    while (linelen > 0 && (line[linelen - 1] == '\n' ||
+                           line[linelen - 1] == '\r'))
+      linelen--;
+
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+  }
+
+  free(line);
+  fclose(fp);
 }
 
 struct abuf {
@@ -296,10 +309,12 @@ void initEditor() {
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   enableRawMode();
   initEditor();
-  editorOpen();
+  if (argc > 2) {
+    editorOpen(argv[1]);
+  }
 
   while(1) {
     editorRefreshScreen();
