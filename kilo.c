bd4f1d7919ffa8653e60c139455c4235463e8db1
algebroid <algebroid0@gmail.com>
2017-09-17 12:04:58 +0900

    Handling Del key, but do nothing for now

--- kilo.c
+++ kilo.c
@@ -16,6 +16,7 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
   HOME_KEY,
   END_KEY,
   PAGE_UP,
@@ -77,6 +78,7 @@ int editorReadKey() {
         if (seq[2] == '~') {
           switch (seq[1]) {
             case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
             case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
