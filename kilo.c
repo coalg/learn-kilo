d8ce8ba338c46215f06a039c61259b080b9166c6
algebroid <algebroid0@gmail.com>
2017-09-17 23:53:21 +0900

    hard code some text for testing

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -144,6 +145,17 @@ int getWindowSize(int *rows, int *cols) {
   *cols = ws.ws_col;
   *rows = ws.ws_row;
   return 0;
+}
+
+void editorOpen() {
+  char *line = "Hello, World!";
+  ssize_t linelen = 43;
+
+  E.row.size = linelen;
+  E.row.chars = malloc(linelen + 1);
+  memcpy(E.row.chars, line, linelen);
+  E.row.chars[linelen] = '\0';
+  E.numrows = 1;
 }
 
 struct abuf {
