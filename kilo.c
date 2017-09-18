db798fa556453c6bccac5632925cc0dda0cdd79e
algebroid <algebroid0@gmail.com>
2017-09-18 12:27:21 +0900

    Add horizonal scrolling (unlimited).

--- kilo.c
+++ kilo.c
@@ -279,9 +279,7 @@ void editorMoveCursor(int key) {
       }
       break;
     case ARROW_RIGHT:
-      if (E.cx != E.screencols - 1) {
-        E.cx++;
-      }
+      E.cx++;
       break;
     case ARROW_UP:
       if (E.cy != 0) {
