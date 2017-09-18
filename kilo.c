6f477ec186a41659d29f0740ee9cb20aae50f95a
algebroid <algebroid0@gmail.com>
2017-09-18 13:56:29 +0900

    Render cursor by rx.

--- kilo.c
+++ kilo.c
@@ -295,7 +295,7 @@ void editorRefreshScreen() {
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
