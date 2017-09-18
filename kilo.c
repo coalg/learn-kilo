c404aae35fa0fad4e26e2f4e346c8ace444f9894
algebroid <algebroid0@gmail.com>
2017-09-18 13:55:56 +0900

    Replace cx by rx.

--- kilo.c
+++ kilo.c
@@ -234,17 +234,19 @@ void abFree(struct abuf *ab) {
 }
 
 void editorScroll() {
+  E.rx = E.cx;
+
   if (E.cy < E.rowoff) {
     E.rowoff = E.cy;
   }
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
-  if (E.cx < E.coloff) {
-    E.coloff = E.cx;
+  if (E.rx < E.coloff) {
+    E.coloff = E.rx;
   }
-  if (E.cx >= E.coloff + E.screencols) {
-    E.coloff = E.cx - E.screencols + 1;
+  if (E.rx >= E.coloff + E.screencols) {
+    E.coloff = E.rx - E.screencols + 1;
   }
 }
 
