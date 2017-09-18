6dfaa373d5621072a40964342303374965f5e58d
algebroid <algebroid0@gmail.com>
2017-09-18 12:21:58 +0900

    Add column offset.

--- kilo.c
+++ kilo.c
@@ -36,6 +36,7 @@ typedef struct erow{
 struct editorConfig {
   int cx, cy;
   int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
   int numrows;
@@ -328,6 +329,7 @@ void initEditor() {
   E.cx = 0;
   E.cy = 0;
   E.rowoff = 0;
+  E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
 
