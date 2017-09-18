d4b927bbad8e44feea9df953dd00dd68ecefa75a
algebroid <algebroid0@gmail.com>
2017-09-18 13:58:59 +0900

    Convert characters index to render index.

--- kilo.c
+++ kilo.c
@@ -157,6 +157,18 @@ int getWindowSize(int *rows, int *cols) {
   return 0;
 }
 
+int editorRowCxToRx(erow *row, int cx) {
+  int rx = 0;
+  int j;
+  for (j = 0; j < cx; j++) {
+    if (row->chars[j] == '\t')
+      rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+    rx++;
+  }
+
+  return rx;
+}
+
 void editorUpdateRow(erow *row) {
   int tabs = 0;
   int j;
