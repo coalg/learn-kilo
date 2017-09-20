8a9d78c16d46eaa61a293cadb9f929aa7d57a3dc
algebroid <algebroid0@gmail.com>
2017-09-21 08:09:11 +0900

    Add function for insert single character.

--- kilo.c
+++ kilo.c
@@ -212,6 +212,15 @@ void editorAppendRow(char *s, size_t len) {
   E.numrows++;
 }
 
+void editorInsertChar(erow *row, int at, int c) {
+  if (at < 0 || at > row->size) at = row->size;
+  row->chars = realloc(row->chars, row->size + 2);
+  memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+  row->size++;
+  row->chars[at] = c;
+  editorUpdateRow(row);
+}
+
 void editorOpen(char* filename) {
   free(E.filename);
   E.filename = strdup(filename);
