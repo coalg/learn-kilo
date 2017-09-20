7beb47075822c48a9173e04e7947b9969444e525
algebroid <algebroid0@gmail.com>
2017-09-20 23:01:01 +0900

    Make room for status bar

--- kilo.c
+++ kilo.c
@@ -324,6 +324,7 @@ void editorDrawStatusBar(struct abuf *ab) {
     }
   }
   abAppend(ab, "\x1b[m", 3);
+  abAppend(ab, "\r\n", 2);
 }
 
 void editorRefreshScreen() {
@@ -451,7 +452,7 @@ void initEditor() {
   E.statusmsg_time = 0;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
-  E.screenrows -= 1;
+  E.screenrows -= 2;
 }
 
 int main(int argc, char *argv[]) {
