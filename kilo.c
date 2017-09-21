44f27e79cb2df5bfcaf75e064de4479f9a037353
algebroid <algebroid0@gmail.com>
2017-09-21 17:06:49 +0900

    Add function for erow to string.

--- kilo.c
+++ kilo.c
@@ -230,6 +230,25 @@ void editorInsertChar(int c) {
   E.cx++;
 }
 
+char *editorRowsToString(int *buflen) {
+  int totlen = 0;
+  int j;
+  for (j = 0; j < E.numrows; j++)
+    totlen += E.row[j].size + 1;
+  *buflen = totlen;
+
+  char *buf = malloc(totlen);
+  char *p = buf;
+  for (j = 0; j < E.numrows; j++) {
+    memcpy(p, E.row[j].chars, E.row[j].size);
+    p += E.row[j].size;
+    *p = '\n';
+    p++;
+  }
+
+  return buf;
+}
+
 void editorOpen(char* filename) {
   free(E.filename);
   E.filename = strdup(filename);
