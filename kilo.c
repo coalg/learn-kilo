35b415a84271e7c36291fcdea962db956eb941f3
algebroid <algebroid0@gmail.com>
2017-09-22 22:58:39 +0900

    add dirty flag

--- kilo.c
+++ kilo.c
@@ -49,6 +49,7 @@ struct editorConfig {
   int screencols;
   int numrows;
   erow *row;
+  int dirty;
   char *filename;
   char statusmsg[80];
   time_t statusmsg_time;
@@ -541,6 +542,7 @@ void initEditor() {
   E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
+  E.dirty = 0;
   E.filename = NULL;
   E.statusmsg[0] = '\0';
   E.statusmsg_time = 0;
