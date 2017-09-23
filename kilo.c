a39f3e0f65df350cf07a49c756103467bacf4257
algebroid <algebroid0@gmail.com>
2017-09-23 22:01:17 +0900

    increment dirty flag.

--- kilo.c
+++ kilo.c
@@ -215,6 +215,7 @@ void editorAppendRow(char *s, size_t len) {
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
+  E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c) {
@@ -224,6 +225,7 @@ void editorRowInsertChar(erow *row, int at, int c) {
   row->size++;
   row->chars[at] = c;
   editorUpdateRow(row);
+  E.dirty++;
 }
 
 void editorInsertChar(int c) {
